c762224afbb9205e75f80d40ebc33e800c02f75f
harshit mahajan <harshit388.be22@chitkara.edu.in>
2024-11-02 14:11:49 +0530

    user verification done

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -33,3 +33,52 @@ export const loginUser = async(req,res) => {
         })
     }
 }
+
+
+
+export const verifyUser = async(req,res) => {
+    try {
+        const {otp, verifyToken} = req.body
+
+        const verify = jwt.verify(verifyToken, process.env.Activation_sec)
+
+        if(!verify) return res.status(400).json({
+            message: "Otp Expired"
+        })
+
+        if(verify.otp !== otp) return res.status(400).json({
+            message: "Wrong Otp"
+        })
+
+        const token = jwt.sign({_id: verify.user._id}, process.env.Jwt_sec, {
+            expiresIn: '5d',
+        })
+
+        res.json({
+            message: "Logged in successfully",
+            user: verify.user,
+            token
+        })
+
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+
+
+export const myProfile = async(req,res) => {
+    try {
+        const user = await User.findById(req.user._id)
+
+        res.json(user)
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
